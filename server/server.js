0bcbe55698274ea89c820f9f22d749f55d430f84
shubhamaity2003 <shubhankarmaity2003@gmail.com>
2026-06-28 23:06:36 +0530

    addin temporary backend server

--- server/server.js
+++ server/server.js
@@ -24,7 +24,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
